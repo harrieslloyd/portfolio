0c91b4465fc89d69fc4a57f9896d419645c6c2e8
Harries Lloyd <harrieslloyd9@gmail.com>
2024-09-24 22:27:52 -0600

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  $(".card").tilt({
+    maxTilt: 5,
+    perspective: 1400,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+    speed: 1200,
+    glare: false,
+    maxGlare: 0,
+    scale: 1.01
+  });
